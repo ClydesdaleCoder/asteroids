c4ea6c1bf389a3bbc3b0ff737885d32bc8c08ac0
ClydesdaleCoder <russellanthonygardner@gmail.com>
2025-03-23 21:05:34 -0700

    3.23 night with asteroids

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import*
 from player import *
+from asteroidfield import *
 
 def main():
     pygame.init()
@@ -9,10 +10,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids,updatable,drawable)
+    AsteroidField.containers = (updatable)
+
     Player(x,y)
+    asteroid_field = AsteroidField()
     
     while True: 
         for event in pygame.event.get():
